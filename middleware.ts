19634cfb308126ff104b99ad50fb47e96ea7ba40
Medina <gustavorsmedina@gmail.com>
2024-12-05 01:42:26 -0300

    feat: autenticando quando faz registro

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,11 @@ import { NextResponse, type NextRequest } from "next/server";
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
 
-  if (!token && request.nextUrl.pathname !== "/login") {
+  if (
+    !token &&
+    request.nextUrl.pathname !== "/login" &&
+    request.nextUrl.pathname !== "/register"
+  ) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
